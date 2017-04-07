914ad16eb1d6e5055d7012498ff8387aac448c0f
omarWCS <omar.mbengue31000@gmail.com>
2017-04-07 09:43:28 +0200

    drawer

---
+++ app/src/main/java/example/com/drawingfun/MainActivity.java
@@ -0,0 +1,28 @@
+package example.com.drawingfun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawingView drawView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawView = (DrawingView)findViewById(R.id.drawing);
+
+
+
+    }
+
+    private void setupDrawing(){
+//get drawing area setup for interaction
+    }
+
+
+}
